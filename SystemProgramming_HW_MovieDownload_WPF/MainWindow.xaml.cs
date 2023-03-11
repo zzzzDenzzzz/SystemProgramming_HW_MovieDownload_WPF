34e05d464903778e377c90df47620361c82c2ea7
zzzzDenzzzz <12den24@gmail.com>
2023-03-11 14:14:48 +0300

    Добавьте файлы проекта.

---
+++ SystemProgramming_HW_MovieDownload_WPF/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media.Animation;
+
+namespace SystemProgramming_HW_MovieDownload_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void DownloadStart(object sender, RoutedEventArgs e)
+        {
+            downloadStatus.BeginAnimation(RangeBase.ValueProperty, new DoubleAnimation(100, new Duration(TimeSpan.FromSeconds(20))));
+        }
+
+        void MessageShow(object sender, RoutedEventArgs e)
+        {
+            new Thread(() => MessageBox.Show("Test")).Start();
+        }
+    }
+}
